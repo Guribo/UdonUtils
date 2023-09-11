8e089c0dd3773544ff65df68b594e5295b57c575
Guribo <guribovr@gmail.com>
2023-09-12 01:41:51 +0200

    feat: remove currently not needed files, convert for VCC

--- Runtime/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("TLP.UdonUtils.Tests.Editor")]
-[assembly: InternalsVisibleTo("TLP.UdonUtils.Tests.Runtime")]
--- Runtime/DesignPatterns/MVC/DummyView.cs
+++
@@ -1,9 +0,0 @@
-namespace TLP.UdonUtils.Runtime.DesignPatterns.MVC
-{
-    public class DummyView : View
-    {
-        public override void OnModelChanged()
-        {
-        }
-    }
-}
